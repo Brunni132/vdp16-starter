501bd73d7d7de07af664e972d6595819b024d7b7
Florian Florian <florian@be-spoke.io>
2020-01-24 11:48:54 +0900

    Hello world

--- src/game-main.js
+++ src/game-main.js
@@ -1,11 +1,20 @@
 import {vdp, color} from "../lib/vdp-lib";
 
 export function *main() {
-	let loop = 0;
 
 	while (true) {
-		vdp.drawObject('level1', 0, loop);
-		loop += 0.2;
+		// Gets the address of the sprite, meaning its x,y position on the sprite sheet,
+		// its w,h (width, height) and some other info.
+		const hello = vdp.sprite('hello');
+
+		// We can either pass 'hello' (name of the sprite) or the sprite address.
+		// As second and third arguments, we pass the x and y position (top-left corner)
+		// We place the top-left corner at the middle of the screen minus half the object
+		// size, making it centered.
+		vdp.drawObject(hello,
+			vdp.screenWidth / 2 - hello.w / 2,
+			vdp.screenHeight / 2 - hello.h / 2);
+
 		yield;
 	}
 }
