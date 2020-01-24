588f7ac0d5e6c4380214a5b336c2b9d74d6da564
Florian Florian <florian@be-spoke.io>
2020-01-24 18:51:22 +0900

    Draw map tuto

--- src/game-main.js
+++ src/game-main.js
@@ -1,22 +1,17 @@
 import {vdp, color} from "../lib/vdp-lib";
 
-// To import the map, we need:
-// 1) To create a palette for it (we could reuse an existing one, but in this case we want a dedicated one). In this example, we'll use 'palette1' which is pre-configured for us.
-// 2) Create a sprite that will hold the tileset. Go to the SPRITES panel, select an empty area using the Rectangle tool. It needs to be big enough to hold all the tiles, which depends on the tile size. You can always create a too big sprite, and then reduce the height after importing the map by coming back to SPRITES, selecting it and reducing the H property. In this case draw a 128x80 rectangle (look at the status bar to check the size).
-// 3) Click on the + button, name the sprite 'level1'.
-// 4) In the right panel that configures the sprite, set TW and TH both to 16 (meaning a tile size of 16x16, what the first level of mario is meant for).
-// 5) This is when you could be starting to draw your tileset directly. In our case, we are going to auto-generate it from importing an already-drawn map.
-// 6) Go to the MAPS panel. Click on the Import button, select the image (level1.png under the gfx directory), select 'level1' as the tileset and click IMPORT. Place the resulting map in any empty area (you can always move it later by doing Ctrl+X / Ctrl+V in Select mode).
-// 7) Your tileset and its associated palette will now contain the colors and 16x16 tiles, and the map can be edited by double-clicking on it.
+// Creating a new map:
+// 1) Create a new sprite. A sprite is used as a tileset, that is, a list of tiles which compose the background, like a puzzle. Go to SPRITES, use the Rect tool, draw a selection (in our case we want 5 tiles of 16x16 each, so we create a rectangle of 80x16) and then click on the + button. Name it 'map2'.
+// 2) Set the TW and TH properties in the right panel to 16, setting up 16x16 tiles.
+// 3) Go back to the Select tool, select the 'map' sprite and double click on it.
+// 4) Use the pen tool to draw some elements: one tile is a ground piece, and two other tiles are a mountain. The fourth tile is a smiley to be used as the sun, and the fifth an empty tile (to leave the background appearing).
+// 5) Go back to the select tool (Escape key). Go to MAPS, draw a selection, in this case 16x16 (the size of the screen is 256x256, which corresponds to 16x16 tiles of 16x16, so a 16x16 map will fill up the screen) and click on the + button to create the map. Name it 'map'.
+// 6) Go back to the Select tool, and double click on the map to edit it.
+// 7) From the right panel, click on the appropriate tiles, and then click on the left map to draw the tile on it. Use right click and draw a selection to copy parts of the map and fill it up quicker.
 
 export function *main() {
-	let scroll = 0;
-
 	while (true) {
-		vdp.drawBackgroundTilemap('level1', { scrollX: scroll, scrollY: -32 });
-		vdp.drawObject('hello', 66, 117);
-		scroll += 0.5;
-
+		vdp.drawBackgroundTilemap('map');
 		yield;
 	}
 }
